4d6ca36643d519535c74e5fef658ea7bcfa7f0f2
mikeyrayvon <mikey@interglobal.vision>
2020-11-19 19:02:43 +0100

    set font family

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,7 @@ class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet" />
           <link rel="icon" href="/favicon.png" type="image/png" />
         </Head>
-        <body className='text-black bg-gray'>
+        <body className='font-sans'>
           <Main />
           <NextScript />
           <PrismicScript />
