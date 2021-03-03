5952f69a3705a25ec4a22d799b5a9299e1c35aec
mikeyrayvon <mikey@interglobal.vision>
2021-03-03 19:57:02 +0100

    add reference to font

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,13 @@
 const Footer = () => {
   return (
     <footer className='py-20'>
+      <Container>
+        <div className='dis-row'>
+          <div className='dis-item mb-4 text-xs'>
+            <span>This website uses <a className='underline' href='https://public-sans.digital.gov/'>Public Sans</a>, a font developed by the U.S. Web Design System for use on federal goverment websites.</span>
+          </div>
+        </div>
+      </Container
     </footer>
   )
 };
