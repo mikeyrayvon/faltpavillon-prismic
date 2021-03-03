9e34e1c011e19e6385499543af85edb74532c7a4
mikeyrayvon <mikey@interglobal.vision>
2021-03-03 20:00:29 +0100

    import container. fix closing tag

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,5 @@
+import Container from './Container'
+
 const Footer = () => {
   return (
     <footer className='py-20'>
@@ -7,7 +9,7 @@ const Footer = () => {
             <span>This website uses <a className='underline' href='https://public-sans.digital.gov/'>Public Sans</a>, a font developed by the U.S. Web Design System for use on federal goverment websites.</span>
           </div>
         </div>
-      </Container
+      </Container>
     </footer>
   )
 };
