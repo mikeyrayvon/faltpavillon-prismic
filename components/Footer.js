bfbde10e0b3b548d162356841e20372ebb56be58
mikeyrayvon <mikey@interglobal.vision>
2021-03-14 21:49:40 +0100

    adjust footer padding

--- components/Footer.js
+++ components/Footer.js
@@ -2,10 +2,10 @@ import Container from './Container'
 
 const Footer = () => {
   return (
-    <footer className='py-20'>
+    <footer className='pt-20 pb-4'>
       <Container>
         <div className='dis-row'>
-          <div className='dis-item mb-4 text-xs'>
+          <div className='dis-item text-xs'>
             <span>This website uses <a className='underline' href='https://public-sans.digital.gov/'>Public Sans</a>, a font developed by the U.S. Web Design System for use on federal goverment websites.</span>
           </div>
         </div>
