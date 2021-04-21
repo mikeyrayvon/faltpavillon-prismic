d28dcfd4d86541931904525e8beb135b66abe2ea
mikeyrayvon <mikey@interglobal.vision>
2021-04-21 12:16:32 +0200

    add sanity schemas

---
+++ studio/schemas/documents/post.js
@@ -0,0 +1,48 @@
+export default {
+  name: 'post',
+  type: 'document',
+  title: 'Post',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      validation: Rule => Rule.required(),
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'content',
+      type: 'portableText',
+      title: 'Body content',
+    },
+    {
+      name: 'seoMeta',
+      title: 'SEO & Metadata',
+      type: 'seoMeta'
+    },
+  ],
+
+  initialValue: {
+    seoMeta: {
+      _type: 'seoMeta',
+      includeInSitemap: true,
+      disallowRobots: false
+    }
+  },
+
+  preview: {
+    select: {
+      title: 'title',
+      subtitle: 'slug.current',
+      media: 'seoMeta.openGraphImage',
+    }
+  }
+}
--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -4,6 +4,16 @@ import createSchema from 'part:@sanity/base/schema-creator'
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
+// Document types
+import post from './documents/post'
+
+// Object types
+import figure from './objects/figure'
+import internalLink from './objects/internalLink'
+import link from './objects/link'
+import portableText from './objects/portableText'
+import seoMeta from './objects/seoMeta'
+
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
@@ -11,6 +21,11 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    figure,
+    internalLink,
+    link,
+    portableText,
+    post,
+    seoMeta
   ]),
 })
