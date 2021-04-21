d0b4a818934d02bad1aee2f4fd93a67c459998fe
mikeyrayvon <mikey@interglobal.vision>
2021-04-21 12:20:15 +0200

    update utils dir

--- web/utils/queries.js
+++ web/utils/queries.js
@@ -1,23 +1,49 @@
-import { Client } from './prismicHelpers'
+import groq from 'groq'
 
-async function fetchDocs(page = 1, routes = []) {
-  const response = await Client().query('', { pageSize: 100, lang: '*', page });
-  const allRoutes = routes.concat(response.results);
-  if (response.results_size + routes.length < response.total_results_size) {
-    return fetchDocs(page + 1, allRoutes);
+export const configQuery = groq`
+  *[_id == "global-config"][0] {
+    ...,
+    headerNotice[]{
+      ...,
+      markDefs[]{
+        ...,
+        _type == 'internalLink' => {
+          _key,
+          'slug': @->slug.current,
+          'type': @->_type
+        },
+        _type == 'actionLink' => {
+          _key,
+          'slug': @->slug.current,
+          'type': @->_type
+        },
+      },
+    },
+    mainNav[] {
+      ...,
+      'title': reference->title,
+      'slug': reference->slug.current,
+      'docType': reference->_type
+    },
+    footerNav {
+      action[] {
+        ...,
+        'title': reference->title,
+        'slug': reference->slug.current,
+        'docType': reference->_type
+      },
+      issues[] {
+        ...,
+        'title': reference->title,
+        'slug': reference->slug.current,
+        'docType': reference->_type
+      },
+      about[] {
+        ...,
+        'title': reference->title,
+        'slug': reference->slug.current,
+        'docType': reference->_type
+      },
+    }
   }
-  return [...new Set(allRoutes)];
-};
-
-/** Fetches all Prismic documents and filters them (eg. by document type).
- *  In production, you would probably query documents by type instead of filtering them.
-**/
-export const queryRepeatableDocuments = async (filter) => {
-  const allRoutes = await fetchDocs()
-  return allRoutes.filter(filter)
-}
-
-export const homePageQuery = async () => {
-  const allRoutes = await fetchDocs()
-  return allRoutes.filter(doc => doc.type === 'post').slice(0, 5)
-}
+`
