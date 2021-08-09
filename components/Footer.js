bc4174a4089a654d284471a2982eecc0a3de258d
mikeyrayvon <mikey@interglobal.vision>
2021-08-09 13:11:53 +0200

    add pro helvetia mention

--- components/Footer.js
+++ components/Footer.js
@@ -2,11 +2,14 @@ import Container from './Container'
 
 const Footer = () => {
   return (
-    <footer className='pt-20 pb-4'>
+    <footer className='pt-20'>
       <Container>
-        <div className='dis-row'>
-          <div className='dis-item text-xs'>
+        <div className='dis-row justify-between'>
+          <div className='dis-item text-xs mb-4'>
             <span>This website uses <a className='underline' href='https://public-sans.digital.gov/'>Public Sans</a>, a font developed by the U.S. Web Design System for use on federal goverment websites.</span>
+          </div>
+          <div className='dis-item text-xs mb-4'>
+            <span>Supported by Pro Helvetia</span>
           </div>
         </div>
       </Container>
